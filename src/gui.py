6a16706b2b8913266c3d78ac5f92c73836913100
Jason Chan <Jason27chan@users.noreply.github.com>
2022-03-18 22:50:52 -0400

    make gui class a bit nicer

--- src/gui.py
+++ src/gui.py
@@ -1,24 +1,27 @@
 import tkinter as tk
 
+from src.file_generator import generate_files_from_csv
+
 
 class Gui:
     @staticmethod
-    def run_main_loop():
-        root = tk.Tk()
-
-        root.title("Chan File Generator")
-
-        container = tk.Frame(root)
-        container.pack()
-
-        message = tk.Text(container)
+    def create_messages(parent):
+        message = tk.Text(parent)
         message.insert(tk.END, "Chan File Generator")
         message.insert(tk.END, "\nPlease press the button")
         message.tag_config("center", justify="center")
         message.tag_add('center', "1.0", "3.0")
         message.pack()
 
-        button = tk.Button(container, text="Generate Files", justify=tk.CENTER, command=generate_files_from_csv)
+    @staticmethod
+    def create_button(parent, command):
+        button = tk.Button(parent, text="Generate Files", justify=tk.CENTER, command=command)
         button.pack()
 
+    @staticmethod
+    def run_main_loop():
+        root = tk.Tk()
+        root.title("Chan File Generator")
+        Gui.create_messages(root)
+        Gui.create_button(root, generate_files_from_csv)
         root.mainloop()
